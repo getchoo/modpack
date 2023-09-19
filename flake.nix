e753014e9b46c3f96abd98871ad4feee555c03f8
seth <getchoo@tuta.io>
2023-09-19 01:07:00 -0400

    feat(nix): use new packwiz2nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,20 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    packwiz2nix.url = "github:getchoo/packwiz2nix";
+    packwiz2nix = {
+      url = "github:getchoo/packwiz2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     packwiz2nix,
+    self,
     ...
   }: let
+    inherit (nixpkgs) lib;
+
     systems = [
       "x86_64-linux"
       "aarch64-linux"
@@ -18,37 +24,36 @@
       "aarch64-darwin"
     ];
 
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
-    inherit (packwiz2nix.lib) mkChecksumsApp mkMultiMCPack mkPackwizPackages;
+    forAllSystems = fn: lib.genAttrs systems (s: fn nixpkgs.legacyPackages.${s});
   in {
-    apps = forAllSystems (s: let
-      pkgs = nixpkgsFor.${s};
-    in {
-      generate-checksums = mkChecksumsApp pkgs ./mods;
-    });
+    packages = forAllSystems (pkgs: let
+      inherit (pkgs.stdenv.hostPlatform) system;
 
-    packages = forAllSystems (s: let
-      pkgs = nixpkgsFor.${s};
-      mods = mkPackwizPackages pkgs ./checksums.json;
-    in rec {
-      getchoo-modpack = mkMultiMCPack {
-        inherit pkgs mods;
-        filesDir = ./files;
-        name = "getchoo-modpack";
+      inherit
+        (packwiz2nix.lib.${system})
+        fetchPackwizModpack
+        mkMultiMCPack
+        ;
+    in {
+      modpack = fetchPackwizModpack {
+        manifest = "${self}/pack.toml";
+        hash = "sha256-NVfgazU/mYs8rGBKNBgV+za+T4tmNLDPA8gz0699ZJs=";
       };
 
-      default = getchoo-modpack;
+      modpack-zip = mkMultiMCPack {
+        src = self.packages.${system}.modpack;
+        instanceCfg = ./files/instance.cfg;
+      };
+
+      default = self.packages.${system}.modpack-zip;
     });
 
-    devShells = forAllSystems (s: let
-      pkgs = nixpkgsFor.${s};
-      inherit (pkgs) mkShell;
-    in {
-      default = mkShell {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
         packages = with pkgs; [
           packwiz
-          p7zip
+          unzip
+          zip
         ];
       };
     });
