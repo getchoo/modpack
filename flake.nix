4ccfccb6a820c3b5de3e17c60ea3621125a8dcae
seth <getchoo@tuta.io>
2024-01-07 23:35:05 -0500

    update to 1.20.4 and move to fabric

--- flake.nix
+++ flake.nix
@@ -1,16 +1,9 @@
 {
   description = "getchoo's modpack";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    packwiz2nix.url = "github:getchoo/packwiz2nix";
-  };
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
-  outputs = {
-    nixpkgs,
-    packwiz2nix,
-    ...
-  }: let
+  outputs = {nixpkgs, ...}: let
     systems = [
       "x86_64-linux"
       "aarch64-linux"
@@ -18,38 +11,11 @@
       "aarch64-darwin"
     ];
 
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
-    inherit (packwiz2nix.lib) mkChecksumsApp mkMultiMCPack mkPackwizPackages;
+    forAllSystems = nixpkgs.lib.genAttrs systems (system: nixpkgs.legacyPackages.${system});
   in {
-    apps = forAllSystems (s: let
-      pkgs = nixpkgsFor.${s};
-    in {
-      generate-checksums = mkChecksumsApp pkgs ./mods;
-    });
-
-    packages = forAllSystems (s: let
-      pkgs = nixpkgsFor.${s};
-      mods = mkPackwizPackages pkgs ./checksums.json;
-    in rec {
-      getchoo-modpack = mkMultiMCPack {
-        inherit pkgs mods;
-        filesDir = ./files;
-        name = "getchoo-modpack";
-      };
-
-      default = getchoo-modpack;
-    });
-
-    devShells = forAllSystems (s: let
-      pkgs = nixpkgsFor.${s};
-      inherit (pkgs) mkShell;
-    in {
-      default = mkShell {
-        packages = with pkgs; [
-          packwiz
-          p7zip
-        ];
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = [pkgs.packwiz];
       };
     });
   };
---
+++ notes.md
@@ -0,0 +1,23 @@
+# Notes on some mods here
+
+This is mainly to keep track of things I would like to add or
+have removed and want back
+
+## Quilt exclusive
+
+Moving from Quilt to Fabric was mainly just to update my game
+version
+
+- LambDynamicLights
+    - Very cool, would like to use again
+- LambdaBetterGrass
+    - Not *as* cool, but would still like to use
+- Inspecio
+    - Probably what I miss most
+- Effective
+    - Also really miss
+
+## Oudated/Unmaintained
+
+- ExtraSounds
+
